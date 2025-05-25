b646c454ed22c707f88a04f3a6e5c7e253295746
Hippolyte Melica <hippolytemelica@gmail.com>
2025-05-25 14:48:07 +0200

    Premier prototype d'asservissement

--- src/main.c
+++ src/main.c
@@ -23,6 +23,49 @@ char    ns_letter     = 0;
 uint8_t vitesse_cible = 50; // en %
 char    state         = dispo;
 
+static const uint16_t coef_correction = 50;
+static const uint16_t inv_amplitude   = 125;
+
+/*
+ * La procédure est la suivante :
+ *  - on calcule à quel point le robot n'est pas sur le fil
+ *  - on cherche à savoir le décalage de la bobine avant (b2) pour déterminer l'accélération de la roue pour tourner
+ *  - on cherche à savoir le décalage de la bobine arrière (b1) pour déterminer la décélération de la roue opposée
+ *    au virage.
+ *  - on en conclue une vitesse de roue, chaque roue par défaut à 512, allant de 0 à 1024.
+ *  - on fait un produit en croix pour remettre à la vitesse maximum.
+ *
+ * Tableau de position en fonction de la phase :
+ *
+ * bobine        | bobine à droite | bobine à gauche
+ * --------------|-----------------|-----------------
+ * avant (b2)    |   opposition    |    en phase
+ * arrière (b1)  |    en phase     |   opposition
+ *
+ * */
+void conducteur() {
+    static uint16_t max_ampl   = 0;
+    uint16_t        mot_droit  = 512;
+    uint16_t        mot_gauche = 512;
+
+    // position du robot sur le fil
+    if (max_ampl < position.com_val)
+        max_ampl = position.com_val;
+    uint16_t div_amplitude = (max_ampl - position.com_val) / inv_amplitude;
+
+    // angle du robot bobine avant
+    if (position.phase & b2) // 1 si en phase, 0 sinon
+        mot_droit += coef_correction * div_amplitude;
+    else
+        mot_gauche += coef_correction * div_amplitude;
+    if (position.phase & b1) // 1 si en phase, 0 sinon
+        mot_droit -= coef_correction * div_amplitude;
+    else
+        mot_gauche -= coef_correction * div_amplitude;
+
+    deplacement(mot_droit, mot_gauche);
+}
+
 int main(void) {
     init_params();
     read_params();
@@ -31,5 +74,6 @@ int main(void) {
     init_moteurs();
     init_bobines();
     init_ir();
+
     while (1) {}
 }
--- src/moteurs.c
+++ src/moteurs.c
@@ -22,8 +22,8 @@ void init_moteurs() {
     LPC_PINCON->PINSEL3 |= 1 << 6 | 1 << 18;
     LPC_MCPWM->MCPER0    = PWM_PERIOD; // période canal 0
     LPC_MCPWM->MCPER1    = PWM_PERIOD; // période canal 1
-    LPC_MCPWM->MCPW0     = PWM_STOP;    // largeur d'impulsion canal 0 (50%)
-    LPC_MCPWM->MCPW1     = PWM_STOP;    // largeur d'impulsion canal 1 (50%)
+    LPC_MCPWM->MCPW0     = PWM_STOP;   // largeur d'impulsion canal 0 (50%)
+    LPC_MCPWM->MCPW1     = PWM_STOP;   // largeur d'impulsion canal 1 (50%)
 
     // Mettre en route les sorties (MCCON_SET)
     LPC_MCPWM->MCCON_SET = (1 << 0) | (1 << 8); // Ch 0A et 1A enable
@@ -36,21 +36,23 @@ void init_moteurs() {
  * vitesse min : 0
  * vitesse max : 512
  * */
+
+static const uint16_t max_commande  = 512;
+static const uint16_t coef_commande = 2;
+
 void deplacement(uint16_t droite, uint16_t gauche) {
     if (droite == 0)
         droite = PWM_STOP;
-    else if (droite >= 512)
+    else if (droite >= max_commande)
         droite = 0;
-    else {
-        droite = 512 - droite;
-    }
+    else
+        droite = max_commande - droite;
     if (gauche == 0)
         gauche = PWM_STOP;
-    else if (gauche >= 512)
+    else if (gauche >= max_commande)
         gauche = 0;
-    else {
-        gauche = 512 - gauche;
-    }
-    LPC_MCPWM->MCPW0 = droite; // largeur d'impulsion canal 0 (50%)
-    LPC_MCPWM->MCPW1 = gauche; // largeur d'impulsion canal 1 (50%)
+    else
+        gauche = max_commande - gauche;
+    LPC_MCPWM->MCPW0 = droite / coef_commande; // largeur d'impulsion canal 0 (50%)
+    LPC_MCPWM->MCPW1 = gauche / coef_commande; // largeur d'impulsion canal 1 (50%)
 }
